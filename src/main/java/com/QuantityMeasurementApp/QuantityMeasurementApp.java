9b6feaa3a5f784f27de6378d80074a859aafa9bb
aryan <124302386+aryannn03@users.noreply.github.com>
2026-03-09 21:24:34 +0530

    [Aryan Malik] Add. UC 5 Unit Conversion

--- src/main/java/com/QuantityMeasurementApp/QuantityMeasurementApp.java
+++ src/main/java/com/QuantityMeasurementApp/QuantityMeasurementApp.java
@@ -2,18 +2,37 @@ package com.QuantityMeasurementApp;
 
 public class QuantityMeasurementApp {
 
+    public static boolean demonstrateLengthEquality(Length l1, Length l2) {
+        return l1.equals(l2);
+    }
+
+    public static double demonstrateLengthConversion(double value,
+                                                     Length.LengthUnit from,
+                                                     Length.LengthUnit to) {
+        return Length.convert(value, from, to);
+    }
+
+    public static Length demonstrateLengthConversion(Length length,
+                                                     Length.LengthUnit to) {
+        return length.convertTo(to);
+    }
+
     public static void main(String[] args) {
 
-        Length l1 = new Length(1.0, Length.LengthUnit.YARDS);
-        Length l2 = new Length(3.0, Length.LengthUnit.FEET);
-        System.out.println("Equal: " + l1.equals(l2));
+        System.out.println(demonstrateLengthConversion(1.0,
+                Length.LengthUnit.FEET,
+                Length.LengthUnit.INCHES));
 
-        Length l3 = new Length(1.0, Length.LengthUnit.YARDS);
-        Length l4 = new Length(36.0, Length.LengthUnit.INCHES);
-        System.out.println("Equal: " + l3.equals(l4));
+        System.out.println(demonstrateLengthConversion(3.0,
+                Length.LengthUnit.YARDS,
+                Length.LengthUnit.FEET));
 
-        Length l5 = new Length(1.0, Length.LengthUnit.CENTIMETERS);
-        Length l6 = new Length(0.393701, Length.LengthUnit.INCHES);
-        System.out.println("Equal: " + l5.equals(l6));
+        System.out.println(demonstrateLengthConversion(36.0,
+                Length.LengthUnit.INCHES,
+                Length.LengthUnit.YARDS));
+
+        System.out.println(demonstrateLengthConversion(1.0,
+                Length.LengthUnit.CENTIMETERS,
+                Length.LengthUnit.INCHES));
     }
 }
