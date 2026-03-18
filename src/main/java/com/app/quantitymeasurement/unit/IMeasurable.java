f3198a96e23704fb9068d72f4477f131ef064241
aryan <124302386+aryannn03@users.noreply.github.com>
2026-03-19 02:23:02 +0530

    [Aryan Malik] Refactor. spring code

---
+++ src/main/java/com/app/quantitymeasurement/unit/IMeasurable.java
@@ -0,0 +1,26 @@
+package com.app.quantitymeasurement.unit;
+
+@FunctionalInterface
+interface SupportsArithmetic{
+    boolean isSupported();
+}
+
+public interface IMeasurable {
+
+    SupportsArithmetic supportsArithmetic=()->true;
+
+    String getUnitName();
+
+    double getConversionFactor();
+
+    double convertToBaseUnit(double value);
+
+    double convertFromBaseUnit(double baseValue);
+
+    default boolean supportsArithmetic(){
+        return supportsArithmetic.isSupported();
+    }
+
+    default void validateOperationSupport(String operation){
+    }
+}
