7b73ed46a9966c80e948e6e9176de0efa35f517d
Aryan Malik <124302386+aryannn03@users.noreply.github.com>
2026-03-30 17:19:31 +0530

    Revert "Feature/uc18 google authnetication and user management"

--- src/main/java/com/app/quantitymeasurement/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.app.quantitymeasurement.repository;
-
-import com.app.quantitymeasurement.entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-}
