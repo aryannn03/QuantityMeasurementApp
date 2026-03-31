d69204cc81b56b18e7a4bb59785d979c46202e52
aryan <124302386+aryannn03@users.noreply.github.com>
2026-04-01 00:55:14 +0530

    [Aryan Malik] Refactor. fixed bugs

--- src/main/java/com/app/quantitymeasurement/controller/QuantityMeasurementController.java
+++ src/main/java/com/app/quantitymeasurement/controller/QuantityMeasurementController.java
@@ -4,39 +4,43 @@ import com.app.quantitymeasurement.dto.QuantityDTO;
 import java.util.List;
 import com.app.quantitymeasurement.entity.QuantityMeasurementEntity;
 import com.app.quantitymeasurement.service.IQuantityMeasurementService;
+
+import jakarta.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/quantities")
+@CrossOrigin(origins = "*")
 public class QuantityMeasurementController {
 
     @Autowired
     private IQuantityMeasurementService service;
 
     @PostMapping("/compare")
-    public boolean compare(@RequestBody QuantityDTO[] quantities){
+    public boolean compare(@Valid @RequestBody QuantityDTO[] quantities){
         return service.compare(quantities[0],quantities[1]);
     }
 
     @PostMapping("/convert")
-    public QuantityDTO convert(@RequestBody QuantityDTO quantity,
+    public QuantityDTO convert(@Valid @RequestBody QuantityDTO quantity,
                                @RequestParam String targetUnit){
         return service.convert(quantity,targetUnit);
     }
 
     @PostMapping("/add")
-    public QuantityDTO add(@RequestBody QuantityDTO[] quantities){
+    public QuantityDTO add(@Valid @RequestBody QuantityDTO[] quantities){
         return service.add(quantities[0],quantities[1]);
     }
 
     @PostMapping("/subtract")
-    public QuantityDTO subtract(@RequestBody QuantityDTO[] quantities){
+    public QuantityDTO subtract(@Valid @RequestBody QuantityDTO[] quantities){
         return service.subtract(quantities[0],quantities[1]);
     }
 
     @PostMapping("/divide")
-    public double divide(@RequestBody QuantityDTO[] quantities){
+    public double divide(@Valid @RequestBody QuantityDTO[] quantities){
         return service.divide(quantities[0],quantities[1]);
     }
     
