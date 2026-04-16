a8afaf82c2d3cca40ee4988b3c968abfe5cdcc83
aryan <124302386+aryannn03@users.noreply.github.com>
2026-04-17 00:17:02 +0530

    [Aryan Malik] Refactor. url fixed

--- auth-service/src/main/java/com/app/auth/security/SecurityConfig.java
+++ auth-service/src/main/java/com/app/auth/security/SecurityConfig.java
@@ -70,7 +70,7 @@ public class SecurityConfig {
         	    "http://127.0.0.1:4200",
         	    "http://localhost:5500",
         	    "http://127.0.0.1:5500",
-        	    "https://quantifyhub.vercel.app/"
+        	    "https://quantifyhub.vercel.app"
         	));
 
         config.setAllowedMethods(List.of(
