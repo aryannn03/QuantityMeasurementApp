663908129461d03ab373419e2514fb1f041d367b
aryan <124302386+aryannn03@users.noreply.github.com>
2026-04-17 01:02:36 +0530

    [Aryan Malik] Refactor. cors fixed

--- auth-service/src/main/java/com/app/auth/security/SecurityConfig.java
+++ auth-service/src/main/java/com/app/auth/security/SecurityConfig.java
@@ -30,7 +30,7 @@ public class SecurityConfig {
 
         http
             .csrf(csrf -> csrf.disable())
-
+            .cors(cors -> cors.configurationSource(corsConfigurationSource()))
             .sessionManagement(session ->
                 session.sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
             )
